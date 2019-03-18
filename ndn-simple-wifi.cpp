917a19c4930dac5fcdff5c6edce4e4a7e7e1d1d1
epicari <aiek261247@gmail.com>
2019-03-18 15:58:17 +0900

    test

--- ndn-simple-wifi.cpp
+++ ndn-simple-wifi.cpp
@@ -123,8 +123,8 @@ main(int argc, char* argv[])
   ndn::AppHelper consumerHelper("ns3::ndn::Producer");
 
   producerHelper.SetPrefix("/");
-  producerHelper.SetAttribute("Frequency", StringValue("10"));
-  producerHelper.SetAttribute("NumberOfContents", StringValue("10"));
+  producerHelper.SetAttribute("Frequency", StringValue("100"));
+  producerHelper.SetAttribute("NumberOfContents", StringValue("100"));
   //consumerHelper.SetAttribute("Batches", StringValue("1s 1"));  
   consumerHelper.SetAttribute("PayloadSize", StringValue("64"));  
   consumerHelper.SetPrefix("/test/prefix");
