1d10c9ca38935abfa74e55b3619b2d3602542182
epicari <aiek261247@gmail.com>
2019-05-29 17:47:36 +0900

    test

--- ndn-simple-wifi.cpp
+++ ndn-simple-wifi.cpp
@@ -154,7 +154,7 @@ main(int argc, char* argv[])
   ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix("/test/prefix");
   consumerHelper.SetAttribute("Frequency", StringValue("1"));
-  ApplicationContainer cunapp = consumerHelper.Install (nodes);
+  ApplicationContainer cunapp = consumerHelper.Install (nodes.Get (1));
 
   ndn::GlobalRoutingHelper::CalculateRoutes();
   Simulator::Stop(Seconds(simTime));
