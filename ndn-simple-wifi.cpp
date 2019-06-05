8f706fdc24ff989467fa2394b9fbd8834d5ceeb1
suho choi <aiek261247@gmail.com>
2019-06-05 10:42:04 +0900

    test

--- ndn-simple-wifi.cpp
+++ ndn-simple-wifi.cpp
@@ -19,13 +19,9 @@
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
-#include "ns3/applications-module.h"
 #include "ns3/wifi-module.h"
-#include "ns3/point-to-point-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/ndnSIM-module.h"
-#include "ns3/csma-helper.h"
-#include "ns3/bridge-helper.h"
 
 using namespace std;
 namespace ns3 {
@@ -35,63 +31,34 @@ NS_LOG_COMPONENT_DEFINE("ndn.WifiExample");
 int
 main(int argc, char* argv[])
 {
-  std::string phyMode = "HtMcs7";
-  //uint16_t disSink = 100;
-  //uint16_t disNode = 35;
+  std::string phyMode = "VhtMcs9";
   uint16_t numberOfnodes = 50;
-  //uint16_t sNode = 2;
-  double txPowerStart = 0.0;
-  double txPowerEnd = 30.0;
   double simTime = 60.0;
 
   CommandLine cmd;
   cmd.Parse(argc, argv);
 
-  // disable fragmentation for frames below 2200 bytes
-  Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold",
-                      StringValue ("2200"));
-  // turn off RTS/CTS for frames below 2200 bytes
-  Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold",
-                      StringValue ("2200"));
-  // Fix non-unicast data rate to be the same as that of unicast
-  //Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode",
-  //                    StringValue (phyMode));
-
-  NodeContainer nodesA;
+  NodeContainer nodes;
   nodesA.Create (numberOfnodes);
-/*
-  NodeContainer nodesB;
-  nodesB.Create (1);
 
-  NodeContainer nodes = NodeContainer (nodesA, nodesB);
-  
-  NodeContainer sinkNode;
-  sinkNode.Create (sNode);
-
-  NodeContainer allNodes = NodeContainer (sinkNode, nodes);
-*/
   ndn::StackHelper ndnHelper;
   ndnHelper.SetOldContentStore ("ns3::ndn::cs::Lru", "MaxSize", "1000");
-  //ndnHelper.SetDefaultRoutes(true);
+  ndnHelper.SetDefaultRoutes(true);
   ndnHelper.InstallAll ();
-  //ndnHelper.Install (sNode);
-  //ndnHelper.Install (nodes);
-
-  //CsmaHelper csma;
-  //NetDeviceContainer backboneDevices = csma.Install (sinkNode);
 
   WifiHelper wifi;
-  wifi.SetStandard(WIFI_PHY_STANDARD_80211n_5GHZ);
+  wifi.SetStandard(WIFI_PHY_STANDARD_80211ac);
   wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager",
                                 "DataMode", StringValue (phyMode), 
-                                "ControlMode", StringValue ("HtMcs0"));
+                                "ControlMode", StringValue ("VhtMcs0"));
   
   YansWifiChannelHelper wifiChannel;
   wifiChannel.SetPropagationDelay("ns3::ConstantSpeedPropagationDelayModel");
-  wifiChannel.AddPropagationLoss("ns3::FriisPropagationLossModel", "Frequency", DoubleValue (5e9));
+  wifiChannel.AddPropagationLoss ("ns3::FixedRssLossModel","Rss",DoubleValue (-77));
 
   YansWifiPhyHelper wifiPhy = YansWifiPhyHelper::Default();
   wifiPhy.SetChannel(wifiChannel.Create());
+/*
   wifiPhy.Set ("TxPowerStart", DoubleValue (txPowerStart));
   wifiPhy.Set ("TxPowerEnd", DoubleValue (txPowerEnd));
   wifiPhy.Set ("TxPowerLevels", UintegerValue (14));
@@ -101,47 +68,18 @@ main(int argc, char* argv[])
   wifiPhy.Set ("CcaMode1Threshold", DoubleValue (-79));
   wifiPhy.Set ("EnergyDetectionThreshold", DoubleValue (-79 + 3));
   wifiPhy.SetErrorRateModel ("ns3::YansErrorRateModel");
-
+*/
   Ssid ssid = Ssid ("wifi-default");
 
   WifiMacHelper wifiMacHelper;
   wifiMacHelper.SetType("ns3::AdhocWifiMac");
-  NetDeviceContainer wifiDev = wifi.Install (wifiPhy, wifiMacHelper, nodesA);
-/*
-  wifiMacHelper.SetType("ns3::StaWifiMac",
-                         "ActiveProbing", BooleanValue (true),
-                         "Ssid", SsidValue (ssid));
-  NetDeviceContainer staDevs = wifi.Install(wifiPhy, wifiMacHelper, nodes);
+  NetDeviceContainer wifiDev = wifi.Install (wifiPhy, wifiMacHelper, nodes);
 
   MobilityHelper mobility;
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-
-  for(uint16_t i = 0; i < sNode; i++)
-    {
-      wifiMacHelper.SetType("ns3::ApWifiMac",
-                            "Ssid", SsidValue (ssid));
-      NetDeviceContainer apDevs = wifi.Install(wifiPhy, wifiMacHelper, sinkNode.Get (i));
-
-      BridgeHelper bridge;
-      NetDeviceContainer bridgeDev = bridge.Install (sinkNode.Get (i), NetDeviceContainer (apDevs, backboneDevices.Get (i)));
-          
-      if(i==0) {
-        positionAlloc->Add (Vector(100, 100, 0));
-        mobility.SetPositionAllocator(positionAlloc);
-        mobility.Install(sinkNode.Get (i));
-      }
-
-      positionAlloc->Add (Vector(120, 120, 0));
-      mobility.SetPositionAllocator(positionAlloc);
-      mobility.Install(sinkNode.Get (i));
-    }
-*/
-  MobilityHelper mobility;
-  //Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
 
   mobility.SetPositionAllocator ("ns3::RandomDiscPositionAllocator",
-                                 "X", StringValue ("100.0"),
-                                 "Y", StringValue ("100.0"),
+                                 "X", StringValue ("500.0"),
+                                 "Y", StringValue ("0.0"),
                                  "Rho", StringValue ("ns3::UniformRandomVariable[Min=0|Max=30]"));
   mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
                                  "Mode", StringValue ("Time"),
@@ -149,61 +87,31 @@ main(int argc, char* argv[])
                                  "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=1.0]"),
                                  "Bounds", StringValue ("0|200|0|200"));
 
-  mobility.Install(nodesA);
-
-  //positionAlloc->Add (Vector(120, 90, 0));
-  //mobility.SetPositionAllocator(positionAlloc);
-  //mobility.Install(nodesB);
-
-/*
-  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
-
-  for (uint16_t i = 0; i < numberOfnodes; i++) {
-    positionAlloc->Add (Vector(disNode * i, 0, 2));
-  }
-
-  MobilityHelper mobility;
-  mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
-  mobility.SetPositionAllocator (positionAlloc); 
-  mobility.Install (nodes);
-
-  for (uint16_t j = 0; j < sNode; j++) {
-    if(j == 0){
-      positionAlloc->Add (Vector(disSink, 0, 6));
-    }
-    else
-      positionAlloc->Add (Vector(disSink * j, 0, 6));    
-  }
-  
-  mobility.SetPositionAllocator (positionAlloc);
-  mobility.Install (sinkNode);
-*/
-
-  ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
-  ndnGlobalRoutingHelper.Install (nodesA);
+  mobility.Install(nodes);
 
   string prefix = "/ucla/hello";
+  //ndn::GlobalRoutingHelper ndnGlobalRoutingHelper;
+  //ndnGlobalRoutingHelper.Install (nodes);
+  //ndnGlobalRoutingHelper.AddOrigins(prefix, nodes);
 
-  ndn::StrategyChoiceHelper::InstallAll(prefix, "/localhost/nfd/strategy/multicast");
-  //ndn::StrategyChoiceHelper::InstallAll(prefix, "/localhost/nfd/strategy/best-route");
+  //ndn::StrategyChoiceHelper::InstallAll(prefix, "/localhost/nfd/strategy/multicast");
+  ndn::StrategyChoiceHelper::InstallAll(prefix, "/localhost/nfd/strategy/best-route");
   
-  ndnGlobalRoutingHelper.AddOrigins(prefix, nodesA);
-
   ndn::AppHelper producerHelper("ns3::ndn::Producer");
   producerHelper.SetPrefix(prefix);
   producerHelper.SetAttribute("PayloadSize", StringValue("1024"));
-  producerHelper.SetAttribute("Freshness", TimeValue(Seconds(5.0))); 
-  producerHelper.Install (nodesA.Get (49));
+  producerHelper.SetAttribute("Freshness", TimeValue(Seconds(30.0))); 
+  producerHelper.Install (nodesA.Get (0));
 
   ndn::AppHelper consumerHelper("ns3::ndn::ConsumerCbr");
   consumerHelper.SetPrefix(prefix);
   consumerHelper.SetAttribute("Frequency", StringValue("10"));
-  consumerHelper.Install (nodesA.Get (0));
+  consumerHelper.Install (nodesA.Get (49));
 
-  ndn::GlobalRoutingHelper::CalculateRoutes();
   Simulator::Stop(Seconds(simTime));
   Simulator::Run();
-
+  
+  //ndn::GlobalRoutingHelper::CalculateRoutes();
   ndn::L3RateTracer::InstallAll("rate-trace.txt", Seconds (1.0));
   ndn::CsTracer::InstallAll("cs-trace.txt", Seconds (1.0));
   ndn::AppDelayTracer::InstallAll("app-delay-tracer.txt");
