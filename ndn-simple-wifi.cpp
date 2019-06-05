01f1393fdc0502a3d7f20e64369055237657ece9
suho choi <aiek261247@gmail.com>
2019-06-05 20:17:05 +0900

    test

--- ndn-simple-wifi.cpp
+++ ndn-simple-wifi.cpp
@@ -98,7 +98,7 @@ main(int argc, char* argv[])
                              "Mode", StringValue ("Time"),
                              "Time", StringValue ("2s"),
                              "Speed", StringValue ("ns3::ConstantRandomVariable[Constant=30.0]"),
-                             "Bounds", StringValue ("0|200|0|200"));
+                             "Bounds", StringValue ("0|400|0|400"));
   mobility.InstallAll ();
 /*
   Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();
